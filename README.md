1a640a2c77cdabc3dcdd2de980ec224bcc88eb55
vuduynhiennn <vuduynhiencute@gmail.com>
2021-11-28 23:52:02 +0700

    add and fix

---
+++ README.md
@@ -0,0 +1,11 @@
+npm init -y
+
+npm i -D tailwindcss postcss autoprefixer vite
+
+npx tailwindcss init -p
+
+npx tailwindcss -i ./css/tailwind.css -o ./build/tailwind.css --watch
+
+npm run dev
+
+source ['click to the link']{https://www.youtube.com/watch?v=_5e42LAPxWw}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  node: "jnit",
+  purge: ["./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        dm: ['"DM Sans"'],
+      },
+      colors: {
+        orangeff9: "#FF9900",
+        orangef8: "F85A47",
+        gray31: "#31353B"
+      }
+  },
+},
+  variants: {
+  extend: { },
+},
+plugins: [],
+}
